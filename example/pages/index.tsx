9801d5a64d5ee2f4e70cbd13c1f4c26777b93721
Tien Pham <tien@phaw.io>
2022-06-18 13:41:54 +0700

    Add preserveSmoothing option

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -15,13 +15,17 @@ import {
   SliderHandle,
   SliderInput,
 } from '@reach/slider'
-import '@reach/slider/styles.css'
 import { Rect } from '@reach/rect'
 import { saveAs } from 'file-saver'
 import copy from 'copy-to-clipboard'
 import toast, { Toaster } from 'react-hot-toast'
 import { getSvgPath, FigmaSquircleParams } from '../../src'
 import GithubCorner from 'react-github-corner'
+import {
+  CustomCheckboxContainer,
+  CustomCheckboxInput,
+  CustomCheckboxProps,
+} from '@reach/checkbox'
 
 export default function Home() {
   return (
@@ -52,6 +56,7 @@ function App() {
   const [size, setSize] = useState(300)
   const [cornerRadius, setCornerRadius] = useState(48)
   const [cornerSmoothing, setCornerSmoothing] = useState(0.8)
+  const [preserveSmoothing, setPreserveSmoothing] = useState(false)
 
   return (
     <div className="flex flex-1 flex-col md:flex-row">
@@ -74,7 +79,7 @@ function App() {
           width={size}
           height={size}
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-current text-violet-600"
+          className="fill-current text-violet-600 absolute"
         >
           <path
             d={`${getSvgPath({
@@ -82,11 +87,11 @@ function App() {
               height: size,
               cornerSmoothing,
               cornerRadius,
+              preserveSmoothing,
             })}`}
           />
         </svg>
       </div>
-
       {/* Sidebar */}
       <div className="flex flex-col w-full bg-gray-800 md:min-h-screen min-h-1/2 md:w-80">
         <div className="hidden md:block">
@@ -102,7 +107,7 @@ function App() {
               label="Size"
               initialValue={300}
               min={100}
-              max={400}
+              max={1000}
               onChange={setSize}
               icon="/icon_size.svg"
             />
@@ -120,6 +125,11 @@ function App() {
             value={cornerSmoothing}
             onChange={setCornerSmoothing}
           />
+          <div className="pt-8 md:pt-12" />
+          <PreserveSmoothingCheckbox
+            checked={preserveSmoothing}
+            onChange={e => setPreserveSmoothing(e.target.checked)}
+          />
         </div>
 
         <div className="flex flex-1" />
@@ -134,6 +144,7 @@ function App() {
                   height: size,
                   cornerRadius,
                   cornerSmoothing,
+                  preserveSmoothing,
                 })
 
                 const blob = new Blob([svg], {
@@ -155,6 +166,7 @@ function App() {
                   height: size,
                   cornerRadius,
                   cornerSmoothing,
+                  preserveSmoothing,
                 })
 
                 copy(svg)
@@ -170,6 +182,28 @@ function App() {
         </div>
       </div>
     </div>
+  )
+}
+
+function PreserveSmoothingCheckbox(props: CustomCheckboxProps) {
+  return (
+    <label className="block flex items-center text-sm text-gray-400 uppercase font-semibold tracking-wider">
+      <CustomCheckboxContainer
+        {...props}
+        className="inline-block relative bg-gray-700 h-4 w-4 rounded outline-none shadow-none mr-2"
+      >
+        <CustomCheckboxInput className="absolute inset-0 opacity-0 z-10" />
+        <span
+          aria-hidden
+          className={`absolute w-full h-full ${
+            props.checked ? 'scale-50' : 'scale-100'
+          } bg-violet-600 rounded ${
+            props.checked ? 'opacity-100' : 'opacity-0'
+          } transition`}
+        />
+      </CustomCheckboxContainer>
+      Preserve smoothing
+    </label>
   )
 }
 
@@ -207,41 +241,39 @@ function NumberInput({
   }
 
   return (
-    <div>
-      <label>
-        <span className="text-sm text-gray-400 uppercase font-semibold tracking-wider">
-          {label}
-        </span>
-        <div className="flex flex-row mt-1 relative block">
-          <SliderLabel
-            value={numericValue}
-            onChange={value => {
-              commitValue(value)
-            }}
-          >
-            <img
-              src={icon}
-              className="h-4 w-4 fill-slate-300"
-              alt=""
-              draggable={false}
-            />
-          </SliderLabel>
-          <input
-            value={value}
-            type="number"
-            onChange={e => setValue(e.target.value)}
-            onBlur={() => commitValue(numericValue)}
-            onKeyUp={e => {
-              if (e.code === 'Enter') {
-                commitValue(numericValue)
-              }
-            }}
-            className="transition py-2 pl-11 pr-3 text-gray-100 bg-gray-700 rounded-lg w-full focus:outline-none focus:border-gray-500 focus:ring-gray-500 focus:ring-1 appearance-none"
+    <label>
+      <span className="text-sm text-gray-400 uppercase font-semibold tracking-wider">
+        {label}
+      </span>
+      <div className="flex flex-row mt-1 relative block">
+        <SliderLabel
+          value={numericValue}
+          onChange={value => {
+            commitValue(value)
+          }}
+        >
+          <img
+            src={icon}
+            className="h-4 w-4 fill-slate-300"
+            alt=""
             draggable={false}
           />
-        </div>
-      </label>
-    </div>
+        </SliderLabel>
+        <input
+          value={value}
+          type="number"
+          onChange={e => setValue(e.target.value)}
+          onBlur={() => commitValue(numericValue)}
+          onKeyUp={e => {
+            if (e.code === 'Enter') {
+              commitValue(numericValue)
+            }
+          }}
+          className="transition py-2 pl-11 pr-3 text-gray-100 bg-gray-700 rounded-lg w-full focus:outline-none focus:border-gray-500 focus:ring-gray-500 focus:ring-1 appearance-none"
+          draggable={false}
+        />
+      </div>
+    </label>
   )
 }
 
@@ -278,7 +310,7 @@ function SliderLabel({ value, onChange, children }: SliderLabelProps) {
 
   return (
     <span
-      className="absolute inset-y-0 left-0 flex items-center pl-4 cursor-ew-resize"
+      className="absolute inset-y-0 left-0 flex items-center pl-4 cursor-ew-resize select-none"
       draggable={false}
       onMouseDown={e => {
         setInitialX(e.clientX)
@@ -372,14 +404,14 @@ function CornerSmoothingSlider({ value, ...rest }: ReachSliderProps) {
           step={0.01}
           {...rest}
         >
-          <SliderTrack>
+          <SliderTrack className="bg-gray-700 h-4 rounded-full">
             <SliderRange />
-            <SliderMarker value={0.6}>
+            <SliderMarker value={0.6} className="bg-gray-500 h-4 w-0.5">
               <div className="w-0 flex justify-center pt-6 select-none">
                 <span className="text-s text-gray-400">iOS</span>
               </div>
             </SliderMarker>
-            <SliderHandle />
+            <SliderHandle className="bg-gray-100 active:bg-gray-400 w-4 h-4 rounded-full focus:outline-none focus:ring focus:ring-gray-500" />
           </SliderTrack>
         </SliderInput>
       </div>
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export interface FigmaSquircleParams {
   cornerSmoothing: number
   width: number
   height: number
+  preserveSmoothing?: boolean
 }
 
 export function getSvgPath({
@@ -18,12 +19,14 @@ export function getSvgPath({
   cornerSmoothing,
   width,
   height,
+  preserveSmoothing = false,
 }: FigmaSquircleParams) {
   const defaultPathParams = getPathParamsForCorner({
     width,
     height,
     cornerRadius,
     cornerSmoothing,
+    preserveSmoothing,
   })
 
   // Most of the time, all corners will have the same radius
@@ -36,6 +39,7 @@ export function getSvgPath({
           height,
           cornerRadius: topLeftCornerRadius,
           cornerSmoothing,
+          preserveSmoothing,
         })
       : defaultPathParams
 
@@ -46,6 +50,7 @@ export function getSvgPath({
           height,
           cornerRadius: topRightCornerRadius,
           cornerSmoothing,
+          preserveSmoothing,
         })
       : defaultPathParams
 
@@ -56,6 +61,7 @@ export function getSvgPath({
           height,
           cornerRadius: bottomRightCornerRadius,
           cornerSmoothing,
+          preserveSmoothing,
         })
       : defaultPathParams
 
@@ -66,6 +72,7 @@ export function getSvgPath({
           height,
           cornerRadius: bottomLeftCornerRadius,
           cornerSmoothing,
+          preserveSmoothing,
         })
       : defaultPathParams
 
@@ -79,6 +86,114 @@ export function getSvgPath({
     .trim()
 }
 
+interface CornerParams {
+  cornerRadius: number
+  cornerSmoothing: number
+  width: number
+  height: number
+  preserveSmoothing: boolean
+}
+
+interface CornerPathParams {
+  a: number
+  b: number
+  c: number
+  d: number
+  p: number
+  cornerRadius: number
+  arcSectionLength: number
+  width: number
+  height: number
+}
+
+// The article from figma's blog
+// https://www.figma.com/blog/desperately-seeking-squircles/
+//
+// The original code by MartinRGB
+// https://github.com/MartinRGB/Figma_Squircles_Approximation/blob/bf29714aab58c54329f3ca130ffa16d39a2ff08c/js/rounded-corners.js#L64
+function getPathParamsForCorner({
+  cornerRadius,
+  cornerSmoothing,
+  width,
+  height,
+  preserveSmoothing,
+}: CornerParams): CornerPathParams {
+  const maxRadius = Math.min(width, height) / 2
+  cornerRadius = Math.min(cornerRadius, maxRadius)
+
+  // From figure 12.2 in the article
+  // p = (1 + cornerSmoothing) * q
+  // in this case q = R because theta = 90deg
+  let p = (1 + cornerSmoothing) * cornerRadius
+
+  // The maximum amount of pixels we can use for rounding and smoothing is maxRadius
+  // When there's not enough space left (p > maxRadius), there are 2 options:
+  //
+  // 1. What figma's currently doing: limit the smoothing value to make sure p <= maxRadius
+  // But what this means is that at some point when cornerRadius is large enough,
+  // increasing the smoothing value wouldn't do anything
+  //
+  // 2. Keep the original smoothing value and use it to calculate the bezier curve normally,
+  // then adjust the control points to achieve similar curvature profile
+  //
+  // preserveSmoothing is a new option I added
+  //
+  // If preserveSmoothing is on then we'll just keep using the original smoothing value
+  // and adjust the bezier curve later
+  if (!preserveSmoothing) {
+    const maxCornerSmoothing = maxRadius / cornerRadius - 1
+    cornerSmoothing = Math.min(cornerSmoothing, maxCornerSmoothing)
+    p = Math.min(p, maxRadius)
+  }
+
+  // In a normal rounded rectangle (cornerSmoothing = 0), this is 90
+  // The larger the smoothing, the smaller the arc
+  const arcMeasure = 90 * (1 - cornerSmoothing)
+  const arcSectionLength =
+    Math.sin(toRadians(arcMeasure / 2)) * cornerRadius * Math.sqrt(2)
+
+  // In the article this is the distance between 2 control points: P3 and P4
+  const angleAlpha = (90 - arcMeasure) / 2
+  const p3ToP4Distance = cornerRadius * Math.tan(toRadians(angleAlpha / 2))
+
+  // a, b, c and d are from figure 11.1 in the article
+  const angleBeta = 45 * cornerSmoothing
+  const c = p3ToP4Distance * Math.cos(toRadians(angleBeta))
+  const d = c * Math.tan(toRadians(angleBeta))
+
+  let b = (p - arcSectionLength - c - d) / 3
+  let a = 2 * b
+
+  // Adjust the P1 and P2 control points if there's not enough space left
+  if (preserveSmoothing && p > maxRadius) {
+    const p1ToP3MaxDistance = maxRadius - d - arcSectionLength - c
+
+    // Try to maintain some distance between P1 and P2 so the curve wouldn't look weird
+    const minA = p1ToP3MaxDistance / 6
+    const maxB = p1ToP3MaxDistance - minA
+
+    b = Math.min(b, maxB)
+    a = p1ToP3MaxDistance - b
+    p = Math.min(p, maxRadius)
+  }
+
+  return {
+    a,
+    b,
+    c,
+    d,
+    p,
+    width,
+    height,
+    arcSectionLength,
+    cornerRadius,
+  }
+}
+
+function toRadians(degrees: number) {
+  return (degrees * Math.PI) / 180
+}
+
 function drawTopRightPath({
   cornerRadius,
   width,
@@ -88,17 +203,17 @@ function drawTopRightPath({
   c,
   d,
   p,
-  circularSectionLength,
+  arcSectionLength,
 }: CornerPathParams) {
   if (cornerRadius) {
     return `
-    M ${Math.max(width / 2, width - p)} 0
+    M ${width - p} 0
     C ${width - (p - a)} 0 ${width - (p - a - b)} 0 ${width -
       (p - a - b - c)} ${d}
-    a ${cornerRadius} ${cornerRadius} 0 0 1 ${circularSectionLength} ${circularSectionLength}
+    a ${cornerRadius} ${cornerRadius} 0 0 1 ${arcSectionLength} ${arcSectionLength}
     C ${width} ${p - a - b}
         ${width} ${p - a}
-        ${width} ${Math.min(height / 2, p)}`
+        ${width} ${p}`
   } else {
     return `M ${width / 2} 0
     L ${width} ${0}
@@ -115,18 +230,18 @@ function drawBottomRightPath({
   c,
   d,
   p,
-  circularSectionLength,
+  arcSectionLength,
 }: CornerPathParams) {
   if (cornerRadius) {
     return `
-    L ${width} ${Math.max(height / 2, height - p)}
+    L ${width} ${height - p}
     C ${width} ${height - (p - a)}
       ${width} ${height - (p - a - b)}
       ${width - d} ${height - (p - a - b - c)}
-    a ${cornerRadius} ${cornerRadius} 0 0 1 -${circularSectionLength} ${circularSectionLength}
+    a ${cornerRadius} ${cornerRadius} 0 0 1 -${arcSectionLength} ${arcSectionLength}
     C ${width - (p - a - b)} ${height}
       ${width - (p - a)} ${height}
-      ${Math.max(width / 2, width - p)} ${height}`
+      ${width - p} ${height}`
   } else {
     return `L ${width} ${height}
     L ${width / 2} ${height}`
@@ -135,25 +250,24 @@ function drawBottomRightPath({
 
 function drawBottomLeftPath({
   cornerRadius,
-  width,
   height,
   a,
   b,
   c,
   d,
   p,
-  circularSectionLength,
+  arcSectionLength,
 }: CornerPathParams) {
   if (cornerRadius) {
     return `
-    L ${Math.min(width / 2, p)} ${height}
+    L ${p} ${height}
     C ${p - a} ${height}
       ${p - a - b} ${height}
       ${p - a - b - c} ${height - d}
-    a ${cornerRadius} ${cornerRadius} 0 0 1 -${circularSectionLength} -${circularSectionLength}
+    a ${cornerRadius} ${cornerRadius} 0 0 1 -${arcSectionLength} -${arcSectionLength}
     C 0 ${height - (p - a - b)}
       0 ${height - (p - a)}
-      0 ${Math.max(height / 2, height - p)}`
+      0 ${height - p}`
   } else {
     return `
     L ${0} ${height}
@@ -163,108 +277,26 @@ function drawBottomLeftPath({
 
 function drawTopLeftPath({
   cornerRadius,
-  width,
-  height,
   a,
   b,
   c,
   d,
   p,
-  circularSectionLength,
+  arcSectionLength,
 }: CornerPathParams) {
   if (cornerRadius) {
     return `
-    L 0 ${Math.min(height / 2, p)}
+    L 0 ${p}
     C 0 ${p - a}
       0 ${p - a - b}
       ${d} ${p - a - b - c}
-    a ${cornerRadius} ${cornerRadius} 0 0 1 ${circularSectionLength} -${circularSectionLength}
+    a ${cornerRadius} ${cornerRadius} 0 0 1 ${arcSectionLength} -${arcSectionLength}
     C ${p - a - b} 0
       ${p - a} 0
-      ${+Math.min(width / 2, p)} 0
+      ${p} 0
     Z`
   } else {
     return `L ${0} ${0}
     Z`
   }
-}
-
-interface CornerParams {
-  cornerRadius: number
-  cornerSmoothing: number
-  width: number
-  height: number
-}
-
-interface CornerPathParams {
-  a: number
-  b: number
-  c: number
-  d: number
-  p: number
-  cornerRadius: number
-  circularSectionLength: number
-  width: number
-  height: number
-}
-
-// The article from figma's blog
-// https://www.figma.com/blog/desperately-seeking-squircles/
-//
-// The original code by MartinRGB
-// https://github.com/MartinRGB/Figma_Squircles_Approximation/blob/bf29714aab58c54329f3ca130ffa16d39a2ff08c/js/rounded-corners.js#L64
-function getPathParamsForCorner({
-  cornerRadius,
-  cornerSmoothing,
-  width,
-  height,
-}: CornerParams): CornerPathParams {
-  const maxRadius = Math.min(width, height) / 2
-  cornerRadius = Math.min(cornerRadius, maxRadius)
-
-  // From figure 12.2 in the article
-  // p = (1 + cornerSmoothing) * q
-  // in this case q = R because theta = 90deg
-  //
-  // Also, when there's not enough space left, we need to back off the smoothing
-  const maxCornerSmoothing = maxRadius / cornerRadius - 1
-  cornerSmoothing = Math.min(cornerSmoothing, maxCornerSmoothing)
-
-  const p = (1 + cornerSmoothing) * cornerRadius
-
-  // These are the angles used in the original code
-  // angleTheta is not the same as theta in the article
-  const angleBeta = 90 * (1 - cornerSmoothing)
-  const angleAlpha = 45 * cornerSmoothing
-  const angleTheta = (90 - angleBeta) / 2
-
-  // This was called `h_longest` in the original code
-  // In the article this is the distance between 2 control points: P3 and P4
-  const p3ToP4Distance = cornerRadius * Math.tan(toRadians(angleTheta / 2))
-
-  // This was called `l` in the original code
-  const circularSectionLength =
-    Math.sin(toRadians(angleBeta / 2)) * cornerRadius * Math.sqrt(2)
-
-  // a, b, c and d are from figure 11.1 in the article
-  const c = p3ToP4Distance * Math.cos(toRadians(angleAlpha))
-  const d = c * Math.tan(toRadians(angleAlpha))
-  const b = (p - circularSectionLength - c - d) / 3
-  const a = 2 * b
-
-  return {
-    a,
-    b,
-    c,
-    d,
-    p,
-    width,
-    height,
-    circularSectionLength,
-    cornerRadius,
-  }
-}
-
-function toRadians(degrees: number) {
-  return (degrees * Math.PI) / 180
 }
